37d2a9b141cb3c861b86e02768b16af5a13c2a5f
Roshan-Devassy <roshandevassy101112@gmail.com>
2025-08-21 20:06:35 +0530

    updated code

--- backend/production.js
+++ backend/production.js
@@ -6,7 +6,9 @@ const multer = require("multer");
 const app = express();
 
 app.use(cors({
-    origin:""
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }))
 app.use(express.json())
 app.use("/uploads", express.static('uploads'))
